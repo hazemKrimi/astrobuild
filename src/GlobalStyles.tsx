6e4e9d9f4fc619f8774e600f5e2d4ee88f8d0cc3
Hazem Krimi <krimihazem1@gmail.com>
2021-04-07 22:04:06 +0100

    Add font and global styles

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+		font-family: 'Inter', monospace;
+		font-size: 16px;
+		line-height: 1.5;
+		outline: none;
+  }
+`;
+
+export default GlobalStyles;
