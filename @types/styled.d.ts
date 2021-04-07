994e8234cda827d84d912fcf3b216c288d8283c3
Hazem Krimi <krimihazem1@gmail.com>
2021-04-07 23:18:36 +0100

    Update app theme

--- @types/styled.d.ts
+++ @types/styled.d.ts
@@ -1,5 +1,44 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-  export interface DefaultTheme {}
+  export interface DefaultTheme {
+    colors: {
+      black: {
+        main: string;
+        light: string;
+      };
+      white: {
+        main: string;
+        light: string;
+      };
+      success: {
+        main: string;
+        light: string;
+      };
+      error: {
+        main: string;
+        light: string;
+      };
+      warning: {
+        main: string;
+        light: string;
+      };
+      client: {
+        main: string;
+        light: string;
+      };
+      productOwner: {
+        main: string;
+        light: string;
+      };
+      developer: {
+        main: string;
+        light: string;
+      };
+      admin: {
+        main: string;
+        light: string;
+      };
+    };
+  }
 }
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,3 +1,70 @@
 import { DefaultTheme } from 'styled-components';
 
-export const theme: DefaultTheme = {};
+export const theme: DefaultTheme = {
+  colors: {
+    black: {
+      main: '#000000',
+      light: '',
+    },
+    white: {
+      main: '#FFFFFF',
+      light: '',
+    },
+    success: {
+      main: '',
+      light: '',
+    },
+    error: {
+      main: '',
+      light: '',
+    },
+    warning: {
+      main: '',
+      light: '',
+    },
+    client: {
+      main: '#5F6CAD',
+      light: `linear-gradient(
+        rgba(255, 255, 255, .75),
+        rgba(255, 255, 255, .75)
+      ),
+      linear-gradient(
+        #5F6CAD,
+        #5F6CAD
+      )`,
+    },
+    productOwner: {
+      main: '#20063B',
+      light: `linear-gradient(
+        rgba(255, 255, 255, .75),
+        rgba(255, 255, 255, .75)
+      ),
+      linear-gradient(
+        #20063B,
+        #20063B
+      )`,
+    },
+    developer: {
+      main: '#ED7D3A',
+      light: `linear-gradient(
+        rgba(255, 255, 255, .75),
+        rgba(255, 255, 255, .75)
+      ),
+      linear-gradient(
+        #ED7D3A,
+        #ED7D3A
+      )`,
+    },
+    admin: {
+      main: '#A30015',
+      light: `linear-gradient(
+        rgba(255, 255, 255, .75),
+        rgba(255, 255, 255, .75)
+      ),
+      linear-gradient(
+        #A30015,
+        #A30015
+      )`,
+    },
+  },
+};
