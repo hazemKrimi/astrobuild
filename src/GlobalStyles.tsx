3ab6713baf974657fbbde48e50e4d4dc4a129a77
Hazem Krimi <krimihazem1@gmail.com>
2021-04-10 00:00:37 +0100

    Update global styles

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -5,7 +5,7 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-		font-family: 'Inter', monospace;
+		font-family: 'Inter', sans-serif;
 		font-size: 16px;
 		line-height: 1.5;
 		outline: none;
