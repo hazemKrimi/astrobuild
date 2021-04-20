52fd3b3be34a6fa7699d91af97247ff4d6cb6bc8
Hazem Krimi <krimihazem1@gmail.com>
2021-04-20 23:23:38 +0100

    Add chevron icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 import { ReactComponent as Add } from './icons/add.svg';
 import { ReactComponent as Upload } from './icons/upload.svg';
+import { ReactComponent as ChevronDown } from './icons/chevron-down.svg';
 
-export { Add, Upload };
+export { Add, Upload, ChevronDown };
