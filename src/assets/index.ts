3e8addb43f2f08be8cf06fe39202467184e2b0a9
Hazem Krimi <krimihazem1@gmail.com>
2021-04-21 00:24:23 +0100

    Add search icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import { ReactComponent as Add } from './icons/add.svg';
 import { ReactComponent as Upload } from './icons/upload.svg';
 import { ReactComponent as ChevronDown } from './icons/chevron-down.svg';
+import { ReactComponent as Search } from './icons/search.svg';
 
-export { Add, Upload, ChevronDown };
+export { Add, Upload, ChevronDown, Search };
