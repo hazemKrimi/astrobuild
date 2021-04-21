ed4f74c9446141ff55e1fb39e6dffc4c001a546c
Hazem Krimi <krimihazem1@gmail.com>
2021-04-21 23:59:35 +0100

    Add check icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,6 @@ import { ReactComponent as Add } from './icons/add.svg';
 import { ReactComponent as Upload } from './icons/upload.svg';
 import { ReactComponent as ChevronDown } from './icons/chevron-down.svg';
 import { ReactComponent as Search } from './icons/search.svg';
+import { ReactComponent as Check } from './icons/check.svg';
 
-export { Add, Upload, ChevronDown, Search };
+export { Add, Upload, ChevronDown, Search, Check };
