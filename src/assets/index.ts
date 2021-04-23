ae78a7afb0a8fee42b905a45564c164fe850581b
Hazem Krimi <krimihazem1@gmail.com>
2021-04-23 18:20:36 +0100

    Add logo and auth page images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,9 @@ import { ReactComponent as Upload } from './icons/upload.svg';
 import { ReactComponent as ChevronDown } from './icons/chevron-down.svg';
 import { ReactComponent as Search } from './icons/search.svg';
 import { ReactComponent as Check } from './icons/check.svg';
+import { ReactComponent as Logo } from './icons/logo.svg';
+import { ReactComponent as Login } from './images/login.svg';
+import { ReactComponent as Signup } from './images/signup.svg';
+import { ReactComponent as Google } from './icons/google.svg';
 
-export { Add, Upload, ChevronDown, Search, Check };
+export { Add, Upload, ChevronDown, Search, Check, Logo, Login, Signup, Google };
