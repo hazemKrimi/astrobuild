f12923649e521b6bd80a8027f9fbc00706bce6b1
Hazem Krimi <krimihazem1@gmail.com>
2021-04-28 21:28:41 +0100

    Add new pages to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,15 @@ import Signup from './Auth/Signup';
 import AdditionalInfo from './Auth/AdditionalInfo';
 import ForgotPassword from './Auth/ForgotPassword';
 import RecoverAccount from './Auth/RecoverAccount';
+import Project from './Project';
+import Main from './Main';
 
-export { Login, Signup, AdditionalInfo, ForgotPassword, RecoverAccount };
+export {
+  Login,
+  Signup,
+  AdditionalInfo,
+  ForgotPassword,
+  RecoverAccount,
+  Project,
+  Main,
+};
