b52ea268935413d8b521bc614457a8731ea0f182
Hazem Krimi <krimihazem1@gmail.com>
2021-04-28 21:25:07 +0100

    Add state reactive variables

--- src/graphql/state.ts
+++ src/graphql/state.ts
@@ -1,1 +1,10 @@
-export {};
+import { makeVar } from '@apollo/client';
+import { UserResponseModel } from './types';
+
+export const tokenVar = makeVar<string | undefined>(undefined);
+
+export const roleVar = makeVar<
+  'client' | 'productOwner' | 'developer' | 'admin' | undefined
+>(undefined);
+
+export const userVar = makeVar<UserResponseModel | undefined>(undefined);
