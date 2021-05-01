26f3ee450fe8ee639e742f5850ff73e07b59b057
Hazem Krimi <krimihazem1@gmail.com>
2021-05-01 17:57:04 +0100

    Add settings page icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,15 @@
 import { ReactComponent as Add } from './icons/add.svg';
 import { ReactComponent as Upload } from './icons/upload.svg';
 import { ReactComponent as ChevronDown } from './icons/chevron-down.svg';
+import { ReactComponent as ArrowLeft } from './icons/arrow-left.svg';
 import { ReactComponent as Search } from './icons/search.svg';
 import { ReactComponent as Check } from './icons/check.svg';
 import { ReactComponent as Google } from './icons/google.svg';
 import { ReactComponent as Settings } from './icons/settings.svg';
 import { ReactComponent as Logout } from './icons/logout.svg';
 import { ReactComponent as Logo } from './icons/logo.svg';
+import { ReactComponent as Profile } from './icons/profile.svg';
+import { ReactComponent as Security } from './icons/security.svg';
 import { ReactComponent as Login } from './images/login.svg';
 import { ReactComponent as Signup } from './images/signup.svg';
 import { ReactComponent as Empty } from './images/empty.svg';
@@ -15,12 +18,15 @@ export {
   Add,
   Upload,
   ChevronDown,
+  ArrowLeft,
   Search,
   Check,
   Google,
   Settings,
   Logout,
   Logo,
+  Profile,
+  Security,
   Login,
   Signup,
   Empty,
