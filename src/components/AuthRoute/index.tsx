969e4d6bb4861d6c4a12e72df58b61cbb6400c13
Hazem Krimi <krimihazem1@gmail.com>
2021-05-01 19:39:59 +0100

    Update auth route component

--- src/components/AuthRoute/index.tsx
+++ src/components/AuthRoute/index.tsx
@@ -1,18 +1,12 @@
 import { useReactiveVar } from '@apollo/client';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
-import { tokenVar, userVar } from '../../graphql/state';
+import { tokenVar } from '../../graphql/state';
 
 const AuthRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
   const token = useReactiveVar(tokenVar);
-  const user = useReactiveVar(userVar);
 
   return (
-    <Route
-      {...rest}
-      render={() =>
-        !token || !user?.firstName ? children : <Redirect to='/' />
-      }
-    />
+    <Route {...rest} render={() => (!token ? children : <Redirect to='/' />)} />
   );
 };
 
