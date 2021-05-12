0d4fc311275ded4ab57ac33612a6c7c11ccd32a5
Hazem Krimi <krimihazem1@gmail.com>
2021-05-12 01:40:11 +0100

    Add prototype page

---
+++ src/pages/Prototype/index.tsx
@@ -0,0 +1,30 @@
+import { useReactiveVar } from '@apollo/client';
+import { Redirect } from 'react-router';
+import { roleVar } from '../../graphql/state';
+import { Empty } from '../../assets';
+import { Box } from '../../components';
+import { Wrapper } from './styles';
+
+const Prototype = () => {
+  const role = useReactiveVar(roleVar);
+
+  return role !== 'admin' ? (
+    <Wrapper color={role}>
+      <Box
+        width='100%'
+        height='100vh'
+        display='grid'
+        alignItems='center'
+        justifyContent='center'
+      >
+        <Box>
+          <Empty />
+        </Box>
+      </Box>
+    </Wrapper>
+  ) : (
+    <Redirect to='/clients' />
+  );
+};
+
+export default Prototype;
